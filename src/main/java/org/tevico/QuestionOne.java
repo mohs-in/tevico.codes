c11c3831ab736a85f6dcd42ce17876cf994be6b3
mohs-in <in.mohsin@outlook.com>
2024-01-07 23:54:35 +0530

    updated code

--- src/main/java/org/tevico/QuestionOne.java
+++ src/main/java/org/tevico/QuestionOne.java
@@ -2,6 +2,10 @@ package org.tevico;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class QuestionOne {
     public static void main(String[] args) {
@@ -12,26 +16,34 @@ public class QuestionOne {
         driver.manage().window().maximize();
 
         try {
+            // Set up WebDriverWait to wait for a max of 10 sec before throwing an exception
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+            WebElement email = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("signinSrEmail")));
+            WebElement password = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("signupSrPassword")));
+            WebElement SignInButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("btn-primary")));
+
             System.out.println("Landed on Login Page");
 
-            WebElement email = driver.findElement(By.id("signinSrEmail"));
+            // Clear any existing text in the email field and enter the specified email address
             email.clear();
             email.sendKeys("tevicouser@gmail.com");
             System.out.println("Entered Email Address");
 
-            WebElement password = driver.findElement(By.id("signupSrPassword"));
+            // Clear any existing password in the password field and enter the specified password
             password.clear();
             password.sendKeys("@10724e2eF");
             System.out.println("Entered Password");
 
-            WebElement SignInButton = driver.findElement(By.className("btn-primary"));
+            // Click on the Sign-In button
             SignInButton.click();
             System.out.println("Clicked on Sign In Button");
 
+            // Successful user Sign-In
             System.out.println("User Signed In Successful.");
 
         } catch (Exception e) {
-            System.out.println("Error!!! Work on it...");
+            System.out.println(e);
         }
     }
 }
